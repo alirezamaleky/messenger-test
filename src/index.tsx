130b015b813e34655571659a7426cfc977742ca7
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-10-20 18:00:26 +0330

    setup react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
+import { createMemoryHistory } from "history";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { Route, Router } from "react-router-dom";
+import { ROUTE_MESSENGER_CHAT } from "./constants/routes";
 import Messenger from "./containers/messenger";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 import MaterialTheme from "./themes";
 
+export const memoryHistory = createMemoryHistory();
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <MaterialTheme>
-        <Messenger />
+        <Router history={memoryHistory}>
+          <Route exact path={ROUTE_MESSENGER_CHAT} component={Messenger} />
+        </Router>
       </MaterialTheme>
     </Provider>
   </React.StrictMode>,
