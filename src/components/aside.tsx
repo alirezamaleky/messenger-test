c9267ade327f002b090b11da601a4f596f2c35bd
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-10-20 18:26:54 +0330

    replace duplicate with loop

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -12,18 +12,6 @@ import styles from "./aside.module.scss";
 import { AvatarSizeType } from "./avatar";
 import UserComponent from "./user";
 
-const TabPanelComponent: FC<any> = (props) => {
-  return (
-    <div hidden={props.value !== props.index} id={`tab-${props.index}`} {...props.other} dir={direction}>
-      {props.value === props.index && (
-        <div className="px-1">
-          <span>{props.children}</span>
-        </div>
-      )}
-    </div>
-  );
-};
-
 enum AsideTabs {
   CONTACTS = 0,
   CHATS = 1,
@@ -40,69 +28,28 @@ const AsideComponent: FC<Props> = (props) => {
     <aside className={`shadow h-100 d-flex flex-column justify-content-start ${styles.aside} ${props.asideStatus ? styles.active : ""}`}>
       <AsideHeader />
       <SwipeableViews className="col p-0" axis={direction === "rtl" ? "x" : "x-reverse"} index={index} onChangeIndex={setIndex}>
-        <TabPanelComponent value={index} index={AsideTabs.CONTACTS}>
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-          <UserComponent initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />
-        </TabPanelComponent>
-        <TabPanelComponent value={index} index={AsideTabs.CHATS}>
-          <UserComponent
-            initials={"A"}
-            unreadCount={2}
-            avatar={undefined}
-            color={colors.indigo["300"]}
-            size={AvatarSizeType.MEDIUM}
-            name={"علیرضا ملکی"}
-            time={"12:58"}
-            message={"لورم ایپسوم متنی ساختگی از صنعت چاپ و گرافیک است."}
-          />
-          <UserComponent
-            initials={"A"}
-            unreadCount={2}
-            avatar={undefined}
-            color={colors.indigo["300"]}
-            size={AvatarSizeType.MEDIUM}
-            name={"علیرضا ملکی"}
-            time={"12:58"}
-            message={"لورم ایپسوم متنی ساختگی از صنعت چاپ و گرافیک است."}
-          />
-          <UserComponent
-            initials={"A"}
-            unreadCount={2}
-            avatar={undefined}
-            color={colors.indigo["300"]}
-            size={AvatarSizeType.MEDIUM}
-            name={"علیرضا ملکی"}
-            time={"12:58"}
-            message={"لورم ایپسوم متنی ساختگی از صنعت چاپ و گرافیک است."}
-          />
-        </TabPanelComponent>
+        <div dir={direction} className="px-1">
+          {Array.from(Array(50), (e, i) => {
+            return <UserComponent key={i} initials={"A"} avatar={undefined} color={colors.indigo["300"]} size={AvatarSizeType.MEDIUM} name={"علیرضا ملکی"} message={"10 دقیقه پیش"} />;
+          })}
+        </div>
+        <div dir={direction} className="px-1">
+          {Array.from(Array(50), (e, i) => {
+            return (
+              <UserComponent
+                key={i}
+                initials={"A"}
+                unreadCount={2}
+                avatar={undefined}
+                color={colors.indigo["300"]}
+                size={AvatarSizeType.MEDIUM}
+                name={"علیرضا ملکی"}
+                time={"12:58"}
+                message={"لورم ایپسوم متنی ساختگی از صنعت چاپ و گرافیک است."}
+              />
+            );
+          })}
+        </div>
       </SwipeableViews>
       <AppBar position="static" className="shadow-lg">
         <Tabs color="primary" indicatorColor="secondary" value={index} onChange={(e, v) => setIndex(v)}>
