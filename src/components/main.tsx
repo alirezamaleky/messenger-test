4cf73566af88586e853e9a1ee7ad5358f294db47
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-10-20 21:15:43 +0330

    editor component by draft-js

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,12 +1,18 @@
 import React, { FC, memo } from "react";
 import MainHeader from "../containers/main-header";
 import { shallowEqual } from "../libs/shallowEqual";
+import MainEditorComponent from "./main-editor";
 import styles from "./main.module.scss";
 
-const MainComponent: FC = () => {
+type Props = {
+  sendMessage: (content: string) => void;
+};
+
+const MainComponent: FC<Props> = (props) => {
   return (
-    <main className={`shadow h-100 d-flex flex-column justify-content-start ${styles.main}`}>
+    <main className={`shadow h-100 d-flex flex-column justify-content-between ${styles.main}`}>
       <MainHeader />
+      <MainEditorComponent sendMessage={props.sendMessage} />
     </main>
   );
 };
