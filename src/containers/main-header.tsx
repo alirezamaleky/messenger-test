41a9f3bd03bd26b7606332bc7febfafd794fcb5d
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-10-20 22:47:39 +0330

    hide main header

--- src/containers/main-header.tsx
+++ src/containers/main-header.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
 import MainHeaderComponent from "../components/main-header";
+import { UserProps } from "../components/user";
 import { shallowEqual } from "../libs/shallowEqual";
 import { ReducersType } from "../store";
 import { toggleAsideStatus } from "../store/theme/actions";
 import { AsideStatusType } from "../store/theme/types";
+import { ChatParamsType } from "./main";
 
 const MainHeader: FC = () => {
   const dispatch = useDispatch();
+  let { id } = useParams<ChatParamsType>();
   const asideStatus = useSelector<ReducersType, AsideStatusType>((state) => state.theme.asideStatus, shallowEqual);
 
-  return <MainHeaderComponent toggleAside={() => dispatch(toggleAsideStatus())} asideStatus={asideStatus} />;
+  const { state } = useLocation();
+  const { initials, avatar, color, name, message } = (state as UserProps) || {};
+
+  return <MainHeaderComponent toggleAside={() => dispatch(toggleAsideStatus())} asideStatus={asideStatus} isInChat={id !== undefined} user={{ initials, avatar, color, name, message }} />;
 };
 
 export default MainHeader;
