5de121b5378928ee67f946e5b78ecfa3823ee6b3
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-10-21 16:44:00 +0330

    Updates

---
+++ precache-manifest.d5a3b9b787947d8719aa9751a7f4b31b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c43e4a19f2dedc71089380dcc955c",
+    "url": "/messenger-test/index.html"
+  },
+  {
+    "revision": "e118d266eb2adf91bd22",
+    "url": "/messenger-test/static/css/main.280ef1b6.chunk.css"
+  },
+  {
+    "revision": "5adde61fb565a725b9b3",
+    "url": "/messenger-test/static/js/2.93d10f71.chunk.js"
+  },
+  {
+    "revision": "3784fd42c06d1a20f1b0f817825666d8",
+    "url": "/messenger-test/static/js/2.93d10f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e118d266eb2adf91bd22",
+    "url": "/messenger-test/static/js/main.8bad8f36.chunk.js"
+  },
+  {
+    "revision": "9734766ecec1a3aebc97",
+    "url": "/messenger-test/static/js/runtime-main.b97b882d.js"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/messenger-test/static/media/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/messenger-test/static/media/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/messenger-test/static/media/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/messenger-test/static/media/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "1becfaba43eef43af02099d64f0590a6",
+    "url": "/messenger-test/static/media/IRANSansWeb_Black.1becfaba.ttf"
+  },
+  {
+    "revision": "c0f5dbbde9104d1dc813b68f50457e2c",
+    "url": "/messenger-test/static/media/IRANSansWeb_Black.c0f5dbbd.eot"
+  },
+  {
+    "revision": "dfb26a2f06ef268a82147b6ea001bcf9",
+    "url": "/messenger-test/static/media/IRANSansWeb_Black.dfb26a2f.woff"
+  },
+  {
+    "revision": "f477dd9634c960c5ce215aa435e32b4c",
+    "url": "/messenger-test/static/media/IRANSansWeb_Black.f477dd96.woff2"
+  },
+  {
+    "revision": "43a0ecf3c7f2af819b192d1284f95ed9",
+    "url": "/messenger-test/static/media/IRANSansWeb_Bold.43a0ecf3.woff2"
+  },
+  {
+    "revision": "e4ed975d7eb3ca8da589f27e55ef7f20",
+    "url": "/messenger-test/static/media/IRANSansWeb_Bold.e4ed975d.woff"
+  },
+  {
+    "revision": "ebec177a9eccde9d5a4fb510ff1d1d8e",
+    "url": "/messenger-test/static/media/IRANSansWeb_Bold.ebec177a.ttf"
+  },
+  {
+    "revision": "f2cb5d5906f5abc3c9f59e561b007ded",
+    "url": "/messenger-test/static/media/IRANSansWeb_Bold.f2cb5d59.eot"
+  },
+  {
+    "revision": "28bc7e37dc1bba427c24941b19a11e4a",
+    "url": "/messenger-test/static/media/IRANSansWeb_Light.28bc7e37.eot"
+  },
+  {
+    "revision": "63f8b4b6f55a927231311a9e83cb4fda",
+    "url": "/messenger-test/static/media/IRANSansWeb_Light.63f8b4b6.woff"
+  },
+  {
+    "revision": "e8ff3f7167961a35e98db2fd15bb37ca",
+    "url": "/messenger-test/static/media/IRANSansWeb_Light.e8ff3f71.ttf"
+  },
+  {
+    "revision": "f7e4be98d20eb763b867143da5207b90",
+    "url": "/messenger-test/static/media/IRANSansWeb_Light.f7e4be98.woff2"
+  },
+  {
+    "revision": "0c40b1a0ee0e7fedeb8a4ac6c58e270b",
+    "url": "/messenger-test/static/media/IRANSansWeb_Medium.0c40b1a0.woff"
+  },
+  {
+    "revision": "1da39fcf35d32c87ce9b3bdcfd2b90b2",
+    "url": "/messenger-test/static/media/IRANSansWeb_Medium.1da39fcf.ttf"
+  },
+  {
+    "revision": "3d468556450b88fba2ef7ac695a166d3",
+    "url": "/messenger-test/static/media/IRANSansWeb_Medium.3d468556.eot"
+  },
+  {
+    "revision": "9c66b762719d40d1f18e678a1405459a",
+    "url": "/messenger-test/static/media/IRANSansWeb_Medium.9c66b762.woff2"
+  },
+  {
+    "revision": "068169d2ab5f8a5dd1beae8b0149217d",
+    "url": "/messenger-test/static/media/IRANSansWeb_UltraLight.068169d2.woff2"
+  },
+  {
+    "revision": "3a70f4b4dcc84bcb84c3749ec235338d",
+    "url": "/messenger-test/static/media/IRANSansWeb_UltraLight.3a70f4b4.eot"
+  },
+  {
+    "revision": "51b9c1ad55d006c89ec0639de7892278",
+    "url": "/messenger-test/static/media/IRANSansWeb_UltraLight.51b9c1ad.ttf"
+  },
+  {
+    "revision": "bb7ec39fffa2e3ce96a5f52edd9e13c4",
+    "url": "/messenger-test/static/media/IRANSansWeb_UltraLight.bb7ec39f.woff"
+  },
+  {
+    "revision": "9f39e76b5a0e039e53afc5d9d4bdd780",
+    "url": "/messenger-test/static/media/messenger-bg.9f39e76b.png"
+  }
+]);
